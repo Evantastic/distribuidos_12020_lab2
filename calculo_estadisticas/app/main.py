cb9852a08e07786a261532d15b3a5f896f28284b
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2020-09-16 16:17:15 -0300

    Agregado prototipo de funcion principal

--- calculo_estadisticas/app/main.py
+++ calculo_estadisticas/app/main.py
@@ -1,11 +1,26 @@
 from distribuidos_common import Cassandra
 from cassandra.util import OrderedMapSerializedKey
 
-c = Cassandra.getInstance()
+def setup():
+    Cassandra.getInstance()
+    Cassandra.addQuery('SELECT info FROM deteccion WHERE objectid=?')
 
-resultado = c.execute('SELECT info FROM deteccion')
 
-for rows in resultado:
-    datos = rows.info
-    fid = datos.get('fid');
-    print(f'fid: {fid}')
+// TODO: Crear clase estadistica e implementarla en esta funcion
+def get_data(objectid):
+    query = Cassandra.query(objectid)
+    green = []
+    red = []
+    for rows in query:
+        info = rows.info;
+        if int(info.get('fid')) == 1:
+            green.append(info)
+        else:
+            red.append(info)
+    return green, red
+
+setup()
+id = 148618231249
+g, r = get_data(id)
+print(f'green: {g}')
+print(f'red: {r}')
