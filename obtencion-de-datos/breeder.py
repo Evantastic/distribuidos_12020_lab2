dc7cdcc995fec6788a16706a8a290ee2298953ef
Cata <Catalina.morales.r@usach.cl>
2020-09-20 00:17:57 -0300

    breeder modificado

--- obtencion-de-datos/breeder.py
+++ obtencion-de-datos/breeder.py
@@ -2,9 +2,7 @@ from confluent_kafka import admin, Producer
 import pandas as pd
 import tarfile
 import fastavro
-import logging
 import wget
-import sys
 import os
 import urllib.request
 import sys
@@ -15,11 +13,6 @@ from itertools import cycle
 
 
 KAFKA_HOST = os.getenv("KAFKA_HOST")
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(name)s.%(funcName)s: %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
-
-ZTF_URL = "https://ztf.uw.edu/alerts/public/"
 WORKING_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 OUTPUT_PATH = os.path.join(WORKING_DIRECTORY)
 KAFKA_CONFIG = {
@@ -49,35 +42,28 @@ def read_avro(file):
         f.seek(0)
         reader = fastavro.reader(f)
         data = reader.next()
-        logging.info(f"Object: {data['objectId']} - {data['candidate']['ra']} {data['candidate']['dec']}")
         return raw
 
 
-def to_stream_tar(file, producer, topic):
+def to_stream_tar(file, producer):
     tar = tarfile.open(os.path.join(OUTPUT_PATH, file))
     particion = cycle(range(4))
     try:
         for member in tar.getmembers():
-            logging.info(f"Getting {member.name}")
             tar.extract(member, OUTPUT_PATH)
             data = read_avro(os.path.join(OUTPUT_PATH, member.name))
             os.remove(os.path.join(OUTPUT_PATH, member.name))
-            producer.produce(topic, value=data, particion = next(particion))
+            producer.produce('Magnitudes', value=data, particion = next(particion))
             producer.flush()
     except Exception as e:
-        logging.error(f"Problem {e} with {file}")
+        pass
     tar.close()
 
 
 def to_stream_dir(path_dir, kafka_client, name):
-    producer = Producer(KAFKA_CONFIG)
     for file in os.listdir(path_dir):
         if file[-6:] == "tar.gz":
-            topic_name = file.split(".")[0]
-            new_topic = admin.NewTopic(topic_name, 1, 1)
-            kafka_client.create_topics([new_topic])
-            logging.info(f"Creating topic {topic_name}")
-            to_stream_tar(file, producer, topic_name)
+            to_stream_tar(file, producer)
     os.remove(OUTPUT_PATH+"/"+name)
 def files():
 	datos = urllib.request.urlopen('https://ztf.uw.edu/alerts/public/').read().decode()
@@ -96,7 +82,7 @@ def new_file(archives):
 schedule.every().day.at("11:00").do(new_file, files())
 
 if __name__ == "__main__":
-    client = admin.AdminClient(KAFKA_CONFIG)
+    producer = Producer(KAFKA_CONFIG)
     while(True):
         schedule.run_pending()
         time.sleep(30)
